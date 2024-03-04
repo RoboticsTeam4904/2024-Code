5622858c7873680ce3e9bd62e6515121034361df
aze12345 <88347612+aze12345@users.noreply.github.com>
2024-03-03 18:06:16 -0800

    auton routine framework

--- src/main/java/org/usfirst/frc4904/robot/Robot.java
+++ src/main/java/org/usfirst/frc4904/robot/Robot.java
@@ -13,13 +13,20 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.Kinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 import static org.usfirst.frc4904.robot.Utils.nameCommand;
+
+import java.util.Map;
 
 import javax.swing.RowFilter.ComparisonType;
 
@@ -30,6 +37,8 @@ public class Robot extends CommandRobotBase {
     private final Operator operator = new DefaultOperator();
     private final RobotMap map = new RobotMap();
     private final CustomCommandJoystick joystick = new CustomCommandJoystick(0, 0.1);
+    private GenericEntry delaySlider;
+
     // public double armJoystick(double input) {
 
     //     return input*30 - Math.signum(input);
@@ -43,7 +52,13 @@ public class Robot extends CommandRobotBase {
     }
 
     @Override
-    public void initialize() {}
+    public void initialize() {
+        delaySlider = Shuffleboard.getTab("Autonomous")
+            .add("Delay", 0) // Default value is 0
+            .withWidget(BuiltInWidgets.kNumberSlider)
+            .withProperties(Map.of("min", 0, "max", 10)) // Set the min and max values for the slider
+            .getEntry();
+    }
 
     @Override
     public void teleopInitialize() {
@@ -74,7 +89,16 @@ public class Robot extends CommandRobotBase {
 
     @Override
     public void autonomousInitialize() {
-        RobotMap.Component.chassis.getAutonomousCommand("line", true).schedule();
+        //TODO create paths
+        var command = new SequentialCommandGroup(
+            new WaitCommand(delaySlider.getDouble(0)),
+            RobotMap.Component.chassis.getAutonomousCommand("start-amp", true),
+            RobotMap.Component.arm.c_holdOuttakeAngle(-1, -1, null),
+            new WaitCommand(1),
+            RobotMap.Component.chassis.getAutonomousCommand("amp-source", true)
+        );
+        command.schedule();
+        
     }
 
     @Override
